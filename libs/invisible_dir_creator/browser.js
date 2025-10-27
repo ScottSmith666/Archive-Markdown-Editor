5ebe620a8b9c34b73e44207b7dbb75b659ad4c0d
Scott Smith <scottsmith999830@gmail.com>
2025-10-28 00:25:21 +0800

    1.0.0

--- libs/invisible_dir_creator/browser.js
+++
@@ -1,1 +0,0 @@
-export * from 'invisible_dir_creator-wasm32-wasi'
--- libs/invisible_dir_creator/test.js
+++
@@ -1,3 +0,0 @@
-let invisible_dir_creator = require("./invisible_dir_creator.node");
-
-invisible_dir_creator.invDirCreator("/Users/scottsmith/Desktop/test");
