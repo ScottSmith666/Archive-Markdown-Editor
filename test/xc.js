f22344f7f21fd4e3a09e8c7d534ae97bfa586901
Scott Smith <scottsmith999830@gmail.com>
2025-11-02 23:28:01 +0800

    1.0.3 alpha4

--- test/xc.js
+++
@@ -1,8 +0,0 @@
-const path = require("node:path");
-const xc_mdz = require(path.join(__dirname, "..", "libs", "rust_libraries", "xc_mdz.node"));
-
-
-xc_mdz.xcMdz("/Users/scottsmith/Desktop/haha", "/Users/scottsmith/Desktop/haha233.mdz", 1, 1, "2333");
-let xx = xc_mdz.xcMdz("/Users/scottsmith/Desktop/haha233.mdz", "/Users/scottsmith/Desktop/haha233", 2, 1, "2333");
-if (xx === -1) console.log("解压失败！");
-else console.log("解压成功！");
